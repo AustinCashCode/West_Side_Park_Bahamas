a31c55ea2e1d1b1a1088e1869c87dfbdb006bb04
Austin Cash <cashau@oregonstate.edu>
2022-11-23 17:19:33 -0800

    Added background image and started flexbox for content

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -5,7 +6,9 @@ function openNav() {
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
   } 
 
-  
+let navBarOpener = document.getElementById("nav-open")
+navBarOpener.addEventListener("click", openNav())
+
