50d94ad1b1933d15290919764c040c3385cf20db
unknown <cashau@oregonstate.edu>
2022-11-23 19:57:41 -0800

    Finished opening/closing of references modal in JS

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ referenceModal.addEventListener("click", () => {
         toShow[i].classList.remove("hidden")
     }
 })
+
+
+let modalClose = document.getElementById("close-button")
+modalClose.addEventListener("click", () => {
+    let backdrop = document.getElementById("modal-backdrop")
+    let myModal = document.getElementById("references-modal")
+
+    backdrop.classList.add("hidden")
+    myModal.classList.add("hidden")
+
+})
