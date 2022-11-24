192cb5d1cd8c970d8b598fc89a73eadbd044be16
unknown <cashau@oregonstate.edu>
2022-11-23 19:52:18 -0800

    added modal and HTML content

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+let referenceModal = document.getElementById("References")
+referenceModal.addEventListener("click", () => {
+    let toShow = document.getElementsByClassName("hidden")
+    for(let i = toShow.length - 1; i >= 0; i--) {
+        toShow[i].classList.remove("hidden")
+    }
+})
